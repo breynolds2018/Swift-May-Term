5de3463107fa154538ba7754b0bbb85e45477203
breynolds2018 <31111349+breynolds2018@users.noreply.github.com>
2018-04-30 10:56:53 -0400

    Add files via upload

---
+++ Day_1_HelloWorld.swift
@@ -0,0 +1,9 @@
+///////////////////
+//Brian Reynolds
+//4/30/18
+//Hello World!
+//////////////////
+
+var myString = "Hello World";
+print(myString);
+
---
+++ Day_1_Vars_Constants.swift
@@ -0,0 +1,31 @@
+////////////
+//Brian Reynolds
+//Day 1
+//Variable and constant delcaration
+//////////
+
+//Constants
+let freezingPoint = 0;
+
+//Variables
+var temp = 15;
+
+////////////Expressing ints in decoimil, Binary, octal, and hex
+
+//Decomil
+var z = 96
+
+//Binary 0b
+var b = 0b1011111
+
+//Octal 0o
+var o = 0o137
+
+//Hex 0x
+var h = 0x5f
+
+////////////////Inilitization without defining 
+var numb: Int
+
+numb = 3
+
