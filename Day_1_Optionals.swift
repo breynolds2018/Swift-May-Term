a6dbf5d6bbabbd448a6f905c51c7a0fe096977d7
breynolds2018 <31111349+breynolds2018@users.noreply.github.com>
2018-04-30 11:32:44 -0400

    Add files via upload

---
+++ Day_1_Optionals.swift
@@ -0,0 +1,20 @@
+////////////////
+//Brian Reynolds
+//Day 1
+//Optionals
+////////////////
+
+//Optional ?
+var optString: String?
+
+optString = nil
+
+//////////Optional Binding
+
+var myOpt: String?
+if let temp = myOpt{
+	print(temp)
+}
+else{
+	print("Nil")
+}
