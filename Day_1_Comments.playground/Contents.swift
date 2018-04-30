96285d1893c5dc9048fce317fecb8c5977780e75
breynolds2018 <31111349+breynolds2018@users.noreply.github.com>
2018-04-30 10:56:29 -0400

    Add files via upload

---
+++ Day_1_Comments.playground/Contents.swift
@@ -0,0 +1,20 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+///
+/// This is where the description of MyFunction goes
+///
+/// - Parameter first: My first parameter
+/// - Parameter second: My second parameter
+/// - Returns: The return of my function
+/// - Throws: Any errors
+///
+func myFunction(first: Int, second: Int) -> Int{
+    // Click option then the function to see description pop up
+    let sum = first+second
+    return sum
+}
+myFunction(first: 1, second: 2)
+
