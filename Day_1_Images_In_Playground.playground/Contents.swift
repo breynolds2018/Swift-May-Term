2186d2b015b07eafe1a749ee9df02411cbd79bcf
breynolds2018 <31111349+breynolds2018@users.noreply.github.com>
2018-04-30 10:55:16 -0400

    Add files via upload

---
+++ Day_1_Images_In_Playground.playground/Contents.swift
@@ -0,0 +1,11 @@
+//: Playground - noun: a place where people can play
+////////////////////
+//Brian Reynolds
+//4/30/18
+//Accessing Images in playgrounds
+///////
+import UIKit
+
+var str = "Hello, playground"
+
+var image = UIImage(named: "IMG_3256.png")
