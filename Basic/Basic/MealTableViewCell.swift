af44a5f276100a32e03fa03f8519b80b64033928
breynolds2018 <31111349+breynolds2018@users.noreply.github.com>
2018-05-31 09:26:54 -0400

    Add files via upload

---
+++ Basic/Basic/MealTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  MealTableViewCell.swift
+//  Basic
+//
+//  Created by Brian Reynolds on 5/23/18.
+//  Copyright © 2018 Brian Reynolds. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+    //MARK: Properties
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
